4187954b10bb67a8d4c461a09446072fdaf07960
Ishan Bhatt <ishanbhatt@halwa>
2019-12-30 11:43:11 +0530

    Added Persistent Volume so that both of the pods give consistent output DO NOT FORGET TO CREATE /mnt/data in YOUR MINIKUBE CLUSTER

--- Prefix2_App/config.py
+++ Prefix2_App/config.py
@@ -1,8 +1,10 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+DB_LOCATION = os.environ["DB_LOCATION"]
+
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'prefix.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DB_LOCATION, 'prefix.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
